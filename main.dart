2afdfd5df6d4665315af175bb18e71ae1f7c0836
José Romualdo <joseromualdo@outlook.com>
2025-08-28 00:57:47 +0000

    feat: add loop

--- main.dart
+++ main.dart
@@ -1,4 +1,18 @@
+import 'dart:io';
 void main(){
-    print("Olá mundo!");
+
+    List<String?> alunos = [];
+    bool cadastrarOutroAluno = true;
+
+    while(cadastrarOutroAluno){
+        print("Informe o nome do aluno:");
+        String? nome = stdin.readLineSync();
+
+        alunos.add(nome);
+
+        print('Deseja cadastrar outro aluno?(S / N)');
+        var respostaUsuario = stdin.readLineSync();
+        cadastrarOutroAluno = respostaUsuario == "S";
+    };    
 }
 
